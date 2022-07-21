dcb59df65ea61c721e50051e68b8caf7199f00df
blu3fire89 <blu3fire89@gmail.com>
2022-07-21 22:16:27 +0800

    update

--- utilities/snapshot.js
+++ utilities/snapshot.js
@@ -2,15 +2,7 @@ const puppeteer = require('puppeteer');
 
 exports.createSnapshot = async({ url }) => {
   try {
-    const browser = await puppeteer.launch({
-      headless: true,
-      args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage',
-        '--single-process'
-      ],
-    });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
     await page.screenshot({path: './uploads/example.png'});
