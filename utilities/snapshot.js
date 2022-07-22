daf470c6540266b9b7cebfbfb84164ffb56f3fcb
BrianSayen <blu3fire89@gmail.com>
2022-07-22 08:18:41 +0800

    update: add params puppeteer

--- utilities/snapshot.js
+++ utilities/snapshot.js
@@ -2,9 +2,18 @@ const puppeteer = require('puppeteer');
 
 exports.createSnapshot = async({ url }) => {
   try {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      headless: true,
+      defaultViewport: null,
+      args: [
+          "--incognito",
+          "--no-sandbox",
+          "--single-process",
+          "--no-zygote"
+      ],
+    });
     const page = await browser.newPage();
-    await page.goto(url);
+    await page.goto(url, {waitUntil: 'domcontentloaded'});
     await page.screenshot({path: './uploads/example.png'});
     let pageDescription;
     let pageTitle;
