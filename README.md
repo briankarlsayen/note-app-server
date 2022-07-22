8bd2d6219b40e8e0fbe7610298bc6802da1c607e
BrianSayen <blu3fire89@gmail.com>
2022-07-22 08:50:27 +0800

    clean

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ npx sequelize db:migrate
 
 ## create new model
 npx sequelize model:generate --name User --attributes firstName:string,lastName:string,email:string
+
+## puppeteer error in heroku
+solution https://stackoverflow.com/questions/52225461/puppeteer-unable-to-run-on-heroku
--- utilities/snapshot.js
+++ utilities/snapshot.js
@@ -1,10 +1,7 @@
 const puppeteer = require('puppeteer');
 
 exports.createSnapshot = async({ url }) => {
-  console.log('url', url)
-  console.log('0')
   try {
-    console.log('1')
     const browser = await puppeteer.launch({
       headless: true,
       defaultViewport: null,
@@ -15,22 +12,17 @@ exports.createSnapshot = async({ url }) => {
           "--no-zygote"
       ],
     });
-    console.log('2')
     const page = await browser.newPage();
-    console.log('3')
     await page.goto(url, {waitUntil: 'domcontentloaded'});
-    console.log('4')
     await page.screenshot({path: './uploads/example.png'});
     let pageDescription;
     let pageTitle;
     let pageImage;
 
     try {
-      console.log('5')
       pageDescription = await page.evaluate(() => document.head.querySelector('meta[property="og:description"]').getAttribute("content"));
       pageTitle = await page.evaluate(() => document.head.querySelector('meta[property="og:title"]').getAttribute("content"));
       pageImage = await page.evaluate(() => document.head.querySelector('meta[property="og:image"]').getAttribute("content"));
-
       const pageInfo = {
         pageTitle,
         pageDescription,
@@ -38,9 +30,7 @@ exports.createSnapshot = async({ url }) => {
       }
       await browser.close();
       return { success: true, ...pageInfo }
-
     } catch(error) {
-      console.log('6')
       await browser.close();
       const pageInfo = {
         pageTitle: null,
@@ -48,27 +38,8 @@ exports.createSnapshot = async({ url }) => {
         pageImage: null,
       }
       return { success: true, ...pageInfo }
-    }
-
-    // if(!pageImage) await page.screenshot({ path: './uploads/example.png' });
-
-
-    // await page.evaluate(() => document.head.querySelector('meta[property="og:description"]').getAttribute("content"))
-    // .then(result => pageDescription = result)
-    // .error(err => pageDescription = null)
-
-    // await page.evaluate(() => document.head.querySelector('meta[property="og:title"]').getAttribute("content"))
-    // .then(result => pageTitle = result)
-    // .error(err => pageTitle = null)
-    
-    // await page.evaluate(() => document.head.querySelector('meta[property="og:image"]').getAttribute("content"))
-    // .then(result => pageImage = result)
-    // .error(err => pageImage = null)
-    // console.log(pageDescription)
-    
-    
+    }    
   } catch(error) {
-    console.log('7')
     return {
       success: false,
       message: "Screenshot failed"
