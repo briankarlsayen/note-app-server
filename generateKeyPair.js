018d7c960dc8fc59bb7ec30bf7e4f7f438171173
blu3fire89 <blu3fire89@gmail.com>
2023-03-18 16:40:12 +0800

    update

--- generateKeyPair.js
+++ generateKeyPair.js
@@ -1,31 +1,34 @@
 /**
  * This module will generate a public and private keypair and save to current directory
- * 
+ *
  * Make sure to save the private key elsewhere after generated!
  */
- const crypto = require('crypto');
- const fs = require('fs');
- 
- function genKeyPair() {
-     
-     // Generates an object where the keys are stored in properties `privateKey` and `publicKey`
-     const keyPair = crypto.generateKeyPairSync('rsa', {
-         modulusLength: 4096, // bits - standard for RSA keys
-         publicKeyEncoding: {
-             type: 'pkcs1', // "Public Key Cryptography Standards 1" 
-             format: 'pem' // Most common formatting choice
-         },
-         privateKeyEncoding: {
-             type: 'pkcs1', // "Public Key Cryptography Standards 1"
-             format: 'pem' // Most common formatting choice
-         }
-     });
+const crypto = require("crypto");
+const fs = require("fs");
 
-    const pubKey = Buffer.from(keyPair.publicKey).toString('base64').replace(/(\r\n|\n|\r)/gm, "");
-    const priKey = Buffer.from(keyPair.privateKey).toString('base64').replace(/(\r\n|\n|\r)/gm, "");
-    let content =  `PUB_KEY=${pubKey}\nPRIV_KEY=${priKey}`
-    
-    fs.writeFileSync(__dirname + '/env.dev', content) // create .env.dev file, stores private key & pub key
- }
+function genKeyPair() {
+  // Generates an object where the keys are stored in properties `privateKey` and `publicKey`
+  const keyPair = crypto.generateKeyPairSync("rsa", {
+    modulusLength: 4096, // bits - standard for RSA keys
+    publicKeyEncoding: {
+      type: "pkcs1", // "Public Key Cryptography Standards 1"
+      format: "pem", // Most common formatting choice
+    },
+    privateKeyEncoding: {
+      type: "pkcs1", // "Public Key Cryptography Standards 1"
+      format: "pem", // Most common formatting choice
+    },
+  });
 
- genKeyPair();
+  const pubKey = Buffer.from(keyPair.publicKey)
+    .toString("base64")
+    .replace(/(\r\n|\n|\r)/gm, "");
+  const priKey = Buffer.from(keyPair.privateKey)
+    .toString("base64")
+    .replace(/(\r\n|\n|\r)/gm, "");
+  let content = `PUB_KEY=${pubKey}\nPRIV_KEY=${priKey}`;
+
+  fs.writeFileSync(__dirname + "/env.dev", content); // create .env.dev file, stores private key & pub key
+}
+
+genKeyPair();
--- models/index.js
+++ models/index.js
@@ -6,17 +6,20 @@ const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 require("dotenv").config();
 const env = process.env.NODE_ENV || "development";
-// const config = require(__dirname + "/../config/config.json")[env];
+const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
-let sequelize = new Sequelize(
-  "postgres://jgmjbmav:amIuUGqWhlBuchoLmbG4AHn4d6qq5EoU@rosie.db.elephantsql.com/jgmjbmav"
-);
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
 
 fs.readdirSync(__dirname)
   .filter((file) => {
