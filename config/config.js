92234f9d0b6e5a51ceabc762bfa7ba23ed9f8309
blu3fire89 <blu3fire89@gmail.com>
2024-04-25 19:45:58 +0800

    update

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 require("dotenv").config();
+const pg = require("pg");
 
 module.exports = {
   development: {
@@ -8,10 +9,12 @@ module.exports = {
     database: "notedb",
     host: "127.0.0.1",
     dialect: "postgres",
+    dialectModule: pg,
   },
   test: {},
   production: {
     dialect: "postgres",
     url: "postgresql://postgres:TFpVAwniOgaGalfVWimVhWqhnOJzGaFI@roundhouse.proxy.rlwy.net:43506/railway", // postres url
+    dialectModule: pg,
   },
 };
